910bf0fe4f2162d4959ee72f42ad9d46851964cd
Peter Souter <peter.souter+GIT@puppet.com>
2016-11-22 00:17:35 +0000

    Remove previous config

--- site/profile/manifests/master.pp
+++ site/profile/manifests/master.pp
@@ -7,23 +7,5 @@ class profile::master {
     enable => true,
   }
 
-  #Lay down update-classes.sh for use in r10k postrun_command
-  #This is configured via the pe_r10k::postrun key in hiera
-  file { '/usr/local/bin/update-classes.sh' :
-    ensure => file,
-    source => 'puppet:///modules/profile/update-classes.sh',
-    mode   => '0755',
-  }
-
-  #https://docs.puppetlabs.com/puppet/latest/reference/config_file_environment.html#environmenttimeout
-  ini_setting { 'environment_timeout = unlimited':
-    ensure  => present,
-    path    => '/etc/puppetlabs/puppet/puppet.conf',
-    section => 'main',
-    setting => 'environment_timeout',
-    value   => 'unlimited',
-    notify  => Service['pe-puppetserver'],
-  }
-
 }
 
